194931cc3c43141c02c57f0ff3225428f13a6817
barungurung00@gmail.com <barun>
2023-11-16 23:55:00 +0000

    EvansScraper update

--- WebScrapperDemo/src/main/java/exampleCode/EvansScrapper.java
+++ WebScrapperDemo/src/main/java/exampleCode/EvansScrapper.java
@@ -17,31 +17,30 @@ public class EvansScrapper {
     }
 
     public static void ScrapeEvans() {
+        for (int j = 1; j <= 5; j++) {
+            try {
 
-        try {
+                Document doc = Jsoup.connect("https://www.evanscycles.com/searchresults?descriptionfilter=bikes#dcp=" + j + "&dppp=60&OrderBy=rank").get();
+                //  Document doc = Jsoup.connect(s).get();
 
-            String itemName = "bike";
-             Document doc = Jsoup.connect("https://www.evanscycles.com/searchresults?descriptionfilter=" + itemName).get();
-          //  Document doc = Jsoup.connect(s).get();
+                //section where bikes are layed out
+                Elements allSection = doc.select(".s-productthumbbox");
 
-            //section where bikes are layed out
-            Elements allSection = doc.select(".s-productthumbbox");
-            
-            System.out.println(allSection.size());
+                System.out.println(allSection.size());
 
-            for (int i = 0; i <= 5; i++) {
+                for (int i = 0; i <= 5; i++) {
 
-                //div for the individual product
-                Elements eachSection = allSection.get(i).select(".s-productthumbbox");
+                    //div for the individual product
+                    Elements eachSection = allSection.get(i).select(".s-productthumbbox");
 
-                //brand name
-                Elements brandName = eachSection.select(".brandWrapTitle");
+                    //brand name
+                    Elements brandName = eachSection.select(".brandWrapTitle");
 
-                //product name
-                Elements productName = eachSection.select(".productdescriptionname");
+                    //product name
+                    Elements productName = eachSection.select(".productdescriptionname");
 
-                //product price
-                Elements productPrice = eachSection.select(".s-largered");
+                    //product price
+                    Elements productPrice = eachSection.select(".s-largered");
 
 // ...
 //                try {
@@ -69,14 +68,15 @@ public class EvansScrapper {
 //                    // Handle SQL exceptions appropriately
 //                    System.out.println("Connection to the database failed. Error message: " + e.getMessage());
 //                }
-
 // ...
-                System.out.println("Cycle name: " + productName.text() + " from " + brandName.text() +" Priced at: "+ productPrice.text());
-                System.out.println("");
-            }
+                    System.out.println("Cycle name: " + productName.text() + " from " + brandName.text() + " Priced at: " + productPrice.text());
+                    System.out.println("");
+                }
 
-        } catch (IOException exc) {
-            System.out.println("Could'nt get the data");
-        }
+            } catch (IOException exc) {
+                System.out.println("Could'nt get the data");
+
+            }
+        }//End of loop of 64 available pages 
     }
 }
