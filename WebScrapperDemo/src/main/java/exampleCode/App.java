7e55057e7128360ce81ea1cebfeba6706b2ec140
barungurung00@gmail.com <barun>
2023-11-18 00:59:22 +0000

    With CyclesUkScrapper it now relates data with relevant tables

--- WebScrapperDemo/src/main/java/exampleCode/App.java
+++ WebScrapperDemo/src/main/java/exampleCode/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args){
     
    // EvansScrapper evansData = new EvansScrapper();
+   //
     CyclesUkScrapper CyclesUkData = new CyclesUkScrapper();
     CyclesUkData.start();
 }
--- WebScrapperDemo/src/main/java/exampleCode/CyclesUkScrapper.java
+++ WebScrapperDemo/src/main/java/exampleCode/CyclesUkScrapper.java
@@ -10,6 +10,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  *
@@ -36,7 +38,7 @@ public class CyclesUkScrapper extends Thread {
     public void run() {
 
         // j is goiing to represent pageNumber 
-        for (int j = 1; j < 2; j++) {
+        for (int j = 1; j < 5; j++) {
 
             try {
 
@@ -49,11 +51,11 @@ public class CyclesUkScrapper extends Thread {
                 Elements mainSection = doc.select(".product-card-custom");
 
                 int price;
-                
+
                 //Display the page number
                 System.out.println("Page ." + j);
                 System.out.println("");
-                for (int i = 0; i < 5; i++) {
+                for (int i = 0; i < mainSection.size(); i++) {
 
                     //Data needed: productName, description, price, 
                     //Individual section
@@ -61,15 +63,16 @@ public class CyclesUkScrapper extends Thread {
 
                     //Product name
                     Elements productName = eachSection.select(".product-title");
+                    String name = productName.text();
 
 //                  product price
 //                  In this website the prices are wrapped with discount so just extracting the current price
                     Elements priceSection = eachSection.select(".product-price");
                     String[] prices = priceSection.text().split(" ");
-                    
+
                     //in allmost all the price sections the first word is the price if not we use condotion
                     String stringPrice = prices[0];
-                    
+
                     if (stringPrice.equals("From")) {
                         stringPrice = prices[1];
                     }
@@ -83,38 +86,138 @@ public class CyclesUkScrapper extends Thread {
                     Elements imgElement = eachSection.select("img");
                     //img url
                     String imgUrl = imgElement.attr("src");
-                    
-                    
+
                     //This is the Url that takes to the page where you get other data like description size color etc
-                    Elements nextPageUrlElement = eachSection.select("a");                  
+                    Elements nextPageUrlElement = eachSection.select("a");
                     String nextPageUrl = "  https://www.cyclesuk.com" + nextPageUrlElement.attr("href");
-                    
+
                     Document nextPageDoc = Jsoup.connect(nextPageUrl).get();
-                    
-                    
+
                     Elements descriptionContainer = nextPageDoc.select("#product-description");
                     Elements paragraph = descriptionContainer.select("p");
                     //there are list of paragraph so we just take the paragraph we need by index
                     String description = paragraph.get(1).text();
-                     
-                  
-                                   
-                   //For bikes table
+                    
+                    //extracting size
+                    Elements sizeSection = nextPageDoc.select(".breadcrumbs");
+                    Elements UnList = sizeSection.select("a");
+                    int lastElement = UnList.size()-1;
+                    Elements li = UnList.get(lastElement).select("a");
+                    
+                    
+
+                    int idBikes = 0;  //primary key for bikes table                 
+                    int idBikeModel = 0; //primary key for bike_model table
+                    int bikes_id; //foreign key for bike_models table 
+                    int bike_models_id;  //foreign key for prices table
+
+                    //For bikes table
+                    try {
+                        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cst3130_cw1", "root", "");
+
+                        if (connection != null && !connection.isClosed()) {
+
+                            // Update the INSERT statement to retrieve generated keys
+                            String insertQuery = "INSERT INTO bikes (name, description) VALUES (?, ?)";
+                            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+
+                            // Set the scraped data as values for the placeholders in the INSERT statement
+                            preparedStatement.setString(1, name);
+                            preparedStatement.setString(2, description);
+
+                            // Execute the INSERT statement
+                            preparedStatement.executeUpdate();
+
+                            // Retrieve the generated keys
+                            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+
+                            if (generatedKeys.next()) {
+                                // Get the auto-generated ID
+                                idBikes = generatedKeys.getInt(1);
+                                //System.out.println("Generated ID: " + id);
+                                // Now you can use the generatedId as needed
+                            } else {
+                                System.out.println("Failed to retrieve generated ID.");
+                            }
+
+                            // Close resources
+                            generatedKeys.close();
+                            preparedStatement.close();
+                            connection.close();
+
+                        } else {
+                            System.out.println("Failed to make a connection to the database.");
+                        }
+
+                    } catch (SQLException e) {
+                        // Handle SQL exceptions appropriately
+                        System.out.println("Connection to the database failed. Error message: " + e.getMessage());
+                    }
+
+                    bikes_id = idBikes;//after data is inserted the id is assigned to foreign key of bike_model table
+                    //System.out.println("primary key for bikes table is " + bikes_id);
+
+                    //For bike_models table
+                    try {
+                        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cst3130_cw1", "root", "");
+
+                        if (connection != null && !connection.isClosed()) {
+
+                            // Update the INSERT statement to retrieve generated keys
+                            String insertQuery = "INSERT INTO bike_models (bikes_id, image) VALUES (?,?)";
+                            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+
+                            // Set the scraped data as values for the placeholders in the INSERT statement
+                            preparedStatement.setInt(1, bikes_id);
+                            preparedStatement.setString(2, imgUrl);
+
+                            // Execute the INSERT statement
+                            preparedStatement.executeUpdate();
+
+                            // Retrieve the generated keys
+                            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+
+                            if (generatedKeys.next()) {
+                                // Get the auto-generated ID
+                                idBikeModel = generatedKeys.getInt(1);
+                                //System.out.println("Generated ID: " + idBikeModel);
+                                // Now you can use the generatedId as needed
+                            } else {
+                                System.out.println("Failed to retrieve generated ID.");
+                            }
+
+                            // Close resources
+                            generatedKeys.close();
+                            preparedStatement.close();
+                            connection.close();
+
+                        } else {
+                            System.out.println("Failed to make a connection to the database.");
+                        }
+
+                    } catch (SQLException e) {
+                        // Handle SQL exceptions appropriately
+                        System.out.println("Connection to the database failed. Error message: " + e.getMessage());
+                    }
+
+                    bike_models_id = idBikeModel;
+                    //System.out.println("primary key for bike_models table is "+bike_models_id);
+
+                    //For prices table
                     try {
                         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cst3130_cw1", "root", "");
                         if (connection != null && !connection.isClosed()) {
 
                             // Perform database operations here if the connection is successful
                             // Update the INSERT statement to use placeholders
-                            String insertQuery = "INSERT INTO bikes (name, description) VALUES ( ?, ?)";
+                            String insertQuery = "INSERT INTO prices (bike_models_id, urls, priceS) VALUES (?, ?, ?)";
                             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
 
                             // Set the scraped data as values for the placeholders in the INSERT statement
-                            preparedStatement.setString(1, productName.text());
-                          //  preparedStatement.setInt(2, price);
-                            preparedStatement.setString(2, description);
+                            preparedStatement.setInt(1, bike_models_id);
+                            preparedStatement.setString(2, nextPageUrl);
+                            preparedStatement.setInt(3, price);
 
-                            //preparedStatement.setString(3, price.text());
                             preparedStatement.executeUpdate();
                             preparedStatement.close();
 
@@ -126,64 +229,6 @@ public class CyclesUkScrapper extends Thread {
                         //Handle SQL exceptions appropriately
                         System.out.println("Connection to the database failed. Error message: " + e.getMessage());
                     }
-                    
-                    
-                    //For bike_models table
-                     try {
-                        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cst3130_cw1", "root", "");
-                        if (connection != null && !connection.isClosed()) {
-
-                            // Perform database operations here if the connection is successful
-                            // Update the INSERT statement to use placeholders
-                            String insertQuery = "INSERT INTO bike_models (image) VALUES (?)";
-                            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-
-                            // Set the scraped data as values for the placeholders in the INSERT statement
-                            preparedStatement.setString(1, imgUrl);
-                            
-//                            preparedStatement.setSting(2, size);
-//                            preparedStatement.setString(2, color);
-
-                            //preparedStatement.setString(3, price.text());
-                            preparedStatement.executeUpdate();
-                            preparedStatement.close();
-
-                            connection.close(); // Close the connection when done
-                        } else {
-                            System.out.println("Failed to make a connection to the database.");
-                        }
-                    } catch (SQLException e) {
-                        //Handle SQL exceptions appropriately
-                        System.out.println("Connection to the database failed. Error message: " + e.getMessage());
-                    }
-                     
-                     
-                     //For prices table
-                      try {
-                        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cst3130_cw1", "root", "");
-                        if (connection != null && !connection.isClosed()) {
-
-                            // Perform database operations here if the connection is successful
-                            // Update the INSERT statement to use placeholders
-                            String insertQuery = "INSERT INTO prices (urls, priceS) VALUES ( ?, ?)";
-                            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-
-                            // Set the scraped data as values for the placeholders in the INSERT statement
-                            preparedStatement.setString(1, nextPageUrl);
-                            preparedStatement.setInt(2, price);
-                                
-                            preparedStatement.executeUpdate();
-                            preparedStatement.close();
-
-                            connection.close(); // Close the connection when done
-                        } else {
-                            System.out.println("Failed to make a connection to the database.");
-                        }
-                    } catch (SQLException e) {
-                        //Handle SQL exceptions appropriately
-                        System.out.println("Connection to the database failed. Error message: " + e.getMessage());
-                    }
-
                 }//End of for loop over the page's list items
 
                 try {
